42a769262937046c28e69d68d0b64414ec46e296
dawidpludowski2000 <dawid_pludowski@wp.pl>
2025-06-20 20:28:33 +0000

    development Edit folder, add Text folder

---
+++ src/components/Text.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Draggable from "react-draggable";
+
+const Text = () => {
+    return <h1>I am text</h1>;
+};
+
+export default Text;
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Text from "../components/Text";
+import { Button } from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
 
 const EditPage = () => {
-    return <div>Edit Page</div>;
+    const [params] = useSearchParams();
+    const [count, setCount] = useState(0);
+
+    const addText = () => {
+        setCount(count + 1)
+    };
+
+    
+
+    return (
+        <div>
+            <div>
+                <img src={params.get("url")} width="250px" />
+                {Array(count)
+                    .fill(0)
+                    .map((e) => (
+                     <Text />
+                    ))}  
+          </div>
+          <Button onClick={addText}>Add Text</Button>
+        </div>
+    );
 };
 
 export default EditPage;
