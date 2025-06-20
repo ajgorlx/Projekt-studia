3a367876d6e9c44e25127b203c84b13322b8c953
Szymon-0 <wrx89834@student.wroclaw.merito.pl>
2025-06-20 18:51:13 +0000

    added api folder, working with api,memes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Homepage from "./pages/Home";
+import EditPage from "./pages/edit";
 
 import './App.css';
 
@@ -11,6 +12,7 @@ function App() {
       <h1>Meme Generator</h1>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/edit" element={<EditPage />} />
       </Routes>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MemeCard from '../components/Card';
+import { GetAllMemes } from '../api/memes';
 
 const Homepage = () => {
+    const [data, SetData] = useState([]);
+
+    useEffect(() => {
+        GetAllMemes().then((memes) => SetData(memes.data.memes));
+    },  []);
+
     return (
-        <div>
-            <MemeCard />
+        <div className="row">
+          {data.map((el) => (
+            <MemeCard img={el.url} title={el.name} />
+        ))}
         </div>
     )
 }
