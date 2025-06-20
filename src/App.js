c471f284c92306b5da6af867e6fe38410cbae47d
Igor Prus <igorprus42@gmail.com>
2025-06-20 16:41:02 +0200

    added imports and instaled react router dom

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {Routes, Route} from "react-router-dom"
+import 'bootstrap/dist/css/booststrap.min.css'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meme Generator</h1>
+      <Routes>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
