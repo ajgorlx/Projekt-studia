85461588e26954e35c10d6980c7b08f63bd989e6
wiktor <wiktor.prokop2@gmail.com>
2025-06-21 12:59:44 +0200

    Wiktor commit

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,11 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, createRef} from 'react';
 import Text from "../components/Text";
 import { Button } from 'react-bootstrap';
 import { useSearchParams } from 'react-router-dom';
+import {exportComponentAsJPEG} from 'react-component-export-image'
 
 const EditPage = () => {
     const [params] = useSearchParams();
     const [count, setCount] = useState(0);
+
+    const memeRef = createRef();
 
     const addText = () => {
         setCount(count + 1)
@@ -15,7 +18,7 @@ const EditPage = () => {
 
     return (
         <div>
-            <div>
+            <div style={{width:"700px", border: '1px solid'}} ref={memeRef} className="meme mt-5 mb-5">
                 <img src={params.get("url")} width="250px" />
                 {Array(count)
                     .fill(0)
@@ -24,6 +27,7 @@ const EditPage = () => {
                     ))}  
           </div>
           <Button onClick={addText}>Add Text</Button>
+          <Button variant="succes" onClick={e => exportComponentAsJPEG(memeRef)}>Save</Button>
         </div>
     );
 };
